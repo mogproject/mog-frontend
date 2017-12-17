1a21aa581139a9a23b573718042a3a382e890b63
Yosuke Mizutani <mogproj@gmail.com>
2017-12-17 12:27:46 -0500

    impl BasePlaygroundState

---
+++ src/main/scala/com/mogproject/mogami/frontend/state/BasePlaygroundState.scala
@@ -0,0 +1,145 @@
+package com.mogproject.mogami.frontend.state
+
+import com.mogproject.mogami._
+import com.mogproject.mogami.frontend.model.{BasePlaygroundModel, CursorFlashRequest, GameInfoDialogRequest, PromotionDialogRequest}
+import com.mogproject.mogami.frontend.sam.{SAMAction, SAMState}
+import com.mogproject.mogami.frontend.view.{BasePlaygroundView, Japanese}
+import com.mogproject.mogami.frontend.view.board.Cursor
+
+/**
+  *
+  */
+trait BasePlaygroundState[M <: BasePlaygroundModel, V <: BasePlaygroundView] extends SAMState[M] {
+  def model: M
+
+  def view: V
+  
+  def adapter(m: M, b: BasePlaygroundModel): M
+
+  def copy(model: M = model, view: V = view): BasePlaygroundState[M, V]
+  
+  override def render(newModel: M): (SAMState[M], Option[SAMAction[M]]) = renderImpl(newModel)
+
+  private[this] def renderImpl(newModel: M, renderAll: Boolean = false): (BasePlaygroundState[M, V], Option[SAMAction[M]]) = {
+
+    val fs: Seq[(Boolean, M => M)] = Seq(
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.flipType.numAreas), renderLayout),
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.pieceWidth), renderSize),
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.recordLang), renderIndex),
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.flipType), renderFlip),
+      (renderAll || isUpdated(newModel, _.config.layout, _.mode.getPlayerNames), renderPlayerNames),
+      (renderAll || isUpdated(newModel, _.config.layout, _.mode.getIndicators), renderIndicators),
+      (renderAll || isUpdated(newModel, _.config.layout, _.mode.boxAvailable), renderBox),
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.pieceFace, _.mode.getBoardPieces), renderBoardPieces),
+      (renderAll || isUpdated(newModel, _.config.layout, _.config.pieceFace, _.mode.getHandPieces), renderHandPieces),
+      (renderAll || isUpdated(newModel, _.config.layout, _.mode.getGameControl.map(_.getDisplayingLastMove)), renderLastMove),
+      (newModel.mode.boxAvailable && (renderAll || isUpdated(newModel, _.config.layout, _.mode.boxAvailable, _.config.pieceFace, _.mode.getBoardPieces, _.mode.getHandPieces)), renderBoxPieces),
+      (renderAll || isUpdated(newModel, _.activeCursor), renderActiveCursor),
+      (renderAll || isUpdated(newModel, _.selectedCursor), renderSelectedCursor),
+      (newModel.renderRequests.nonEmpty, processRenderRequests)
+    )
+
+    val nextModel = fs.foldLeft(newModel) { case (m, (cond, f)) => if (cond) f(m) else m }
+    if (newModel.mode.isJustMoved(model.mode)) renderMove(newModel)
+    (this.copy(model = nextModel), None)
+  }
+
+  override def initialize(): Unit = {
+    renderImpl(model, renderAll = true)
+  }
+
+  //
+  // Utilities
+  //
+  private[this] def isUpdated[A](newModel: M, fs: (M => A)*): Boolean = {
+    fs.exists(f => f(model) != f(newModel))
+  }
+
+  //
+  // Rendering functions
+  //
+  private[this] def renderLayout(newModel: M): M = {
+    view.renderLayout(newModel.config.flipType.numAreas, newModel.config.layout)
+    newModel
+  }
+
+  private[this] def renderSize(newModel: M): M = {
+    view.renderSize(newModel.config.getPieceWidth)
+    newModel
+  }
+
+  private[this] def renderIndex(newModel: M): M = {
+    view.renderIndex(newModel.config.recordLang == Japanese)
+    newModel
+  }
+
+  private[this] def renderFlip(newModel: M): M = {
+    view.renderFlip(newModel.config.flipType)
+    newModel
+  }
+
+  private[this] def renderPlayerNames(newModel: M): M = {
+    view.renderPlayerNames(newModel.mode.getPlayerNames, newModel.config.messageLang)
+    newModel
+  }
+
+  private[this] def renderIndicators(newModel: M): M = {
+    view.renderIndicators(newModel.mode.getIndicators)
+    newModel
+  }
+
+  private[this] def renderBox(newModel: M): M = {
+    view.renderBox(newModel.mode.boxAvailable)
+    newModel
+
+  }
+
+  private[this] def renderBoardPieces(newModel: M): M = {
+    view.renderBoardPieces(newModel.mode.getBoardPieces, newModel.config.pieceFace)
+    newModel
+  }
+
+  private[this] def renderHandPieces(newModel: M): M = {
+    view.renderHandPieces(newModel.mode.getHandPieces, newModel.config.pieceFace)
+    newModel
+  }
+
+  private[this] def renderLastMove(newModel: M): M = {
+    view.renderLastMove(newModel.mode.getLastMove)
+    newModel
+  }
+
+  private[this] def renderBoxPieces(newModel: M): M = {
+    view.renderBoxPieces(newModel.mode.getBoxPieces, newModel.config.pieceFace)
+    newModel
+  }
+
+  private[this] def renderActiveCursor(newModel: M): M = {
+    view.renderActiveCursor(newModel.activeCursor)
+    newModel
+  }
+
+  private[this] def renderSelectedCursor(newModel: M): M = {
+    val attack = for {
+      c <- newModel.selectedCursor.toSet if newModel.config.visualEffectEnabled
+      from = c.moveFrom
+      atk <- newModel.mode.getLegalMoves(from)
+    } yield atk
+    view.renderSelectedCursor(newModel.selectedCursor, newModel.config.visualEffectEnabled, attack)
+    newModel
+  }
+
+  private[this] def processRenderRequests(newModel: M): M = {
+    newModel.renderRequests.foreach {
+      case PromotionDialogRequest(rawMove: Move) => ???
+      case CursorFlashRequest(cursor: Cursor) => view.mainPane.updateSVGArea(_.flashCursor(cursor))
+      case GameInfoDialogRequest => ???
+    }
+    adapter(newModel, newModel.copy(newRenderRequests = Seq.empty))
+  }
+
+  private[this] def renderMove(newModel: M): Unit = {
+    newModel.mode.getLastMove.foreach(view.renderMoveEffect(_, newModel.config.pieceFace, newModel.config.visualEffectEnabled, newModel.config.soundEffectEnabled))
+  }
+
+}
