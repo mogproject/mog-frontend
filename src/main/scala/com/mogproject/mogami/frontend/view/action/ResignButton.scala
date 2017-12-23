c7085881dfd83347a05627f9367f9f291e401542
Yosuke Mizutani <mogproj@gmail.com>
2017-12-23 03:15:08 -0500

    hide menu dialog after resign

--- src/main/scala/com/mogproject/mogami/frontend/view/action/ResignButton.scala
+++ src/main/scala/com/mogproject/mogami/frontend/view/action/ResignButton.scala
@@ -6,6 +6,7 @@ import com.mogproject.mogami.frontend.view.button.SingleButton
 import com.mogproject.mogami.frontend.view.modal.YesNoDialog
 import com.mogproject.mogami.frontend.view.observer.ModeObserver
 import com.mogproject.mogami.frontend._
+import com.mogproject.mogami.frontend.action.dialog.MenuDialogAction
 import com.mogproject.mogami.frontend.model.Mode
 import org.scalajs.dom.raw.HTMLElement
 
@@ -24,19 +25,24 @@ case class ResignButton(isSmall: Boolean, confirm: Boolean) extends WebComponent
     }
   }
 
+  /** @note `dismissModal = true` might not work because of `handleUpdate` */
   private[this] val button = SingleButton(
     Map(English -> createLabel(English), Japanese -> createLabel(Japanese)),
     "btn-default" +: Seq("thin-btn", "btn-resign").filter(_ => isSmall),
     clickAction = Some(() => clickAction()),
     tooltip = if (isSmall) Map.empty else Map(English -> "Resign this game", Japanese -> "投了します"),
-    isBlockButton = !isSmall,
-    dismissModal = !isSmall
+    isBlockButton = !isSmall
   )
 
   def clickAction(): Unit = if (confirm) {
-    YesNoDialog(English, div("Do you really want to resign?"), () => doAction(ResignAction)).show()
+    YesNoDialog(English, div("Do you really want to resign?"), () => clickActionImpl()).show()
   } else {
+    clickActionImpl()
+  }
+
+  def clickActionImpl(): Unit = {
     doAction(ResignAction)
+    if (!isSmall) doAction(MenuDialogAction(false))
   }
 
   override def handleUpdate(mode: Mode): Unit = setDisabled(!mode.canMakeMove)
