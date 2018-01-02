e88a06e286b8795e5fb58dca8355c71d96b3577e
Yosuke Mizutani <mogproj@gmail.com>
2018-01-02 01:22:57 -0500

    impl Edit attribute buttons

---
+++ src/main/scala/com/mogproject/mogami/frontend/action/board/EditAttributeAction.scala
@@ -0,0 +1,24 @@
+package com.mogproject.mogami.frontend.action.board
+
+import com.mogproject.mogami._
+import com.mogproject.mogami.util.Implicits._
+import com.mogproject.mogami.frontend.action.PlaygroundAction
+import com.mogproject.mogami.frontend.model.{BasePlaygroundModel, EditMode}
+import com.mogproject.mogami.frontend.model.board.cursor.BoardCursor
+
+/**
+  * Change attributes of a selected piece in Edit Mode
+  */
+case class EditAttributeAction(promoted: Boolean, rotated: Boolean) extends PlaygroundAction {
+  override def execute(model: BasePlaygroundModel): Option[BasePlaygroundModel] = {
+    (model.mode, model.selectedCursor) match {
+      case (m@EditMode(_, _, b, _), Some((areaId, BoardCursor(sq)))) =>
+        b.get(sq).map { p =>
+          val owner = (model.config.isAreaFlipped(areaId) ^ rotated).fold(WHITE, BLACK)
+          val ptype = promoted.fold(p.ptype.promoted, p.ptype.demoted)
+          model.copy(newMode = m.copy(board = b.updated(sq, Piece(owner, ptype))), newSelectedCursor = None)
+        }
+      case _ => None
+    }
+  }
+}
